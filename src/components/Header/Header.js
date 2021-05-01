83f876df520d07a1e248f17f343e8cf359f3c2d2
sabbir hossain <shr472978@gmail.com>
2021-05-02 00:42:13 +0600

    Changed CssBase

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavCom from "./NavCom";
 import MoveStuffAround from "../Body/Ticker";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import { CssBaseline } from "@material-ui/core";
 import Banner from "../Body/Banner";
 
 const Header = () => {
