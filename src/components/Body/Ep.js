1cf5bb18419b90559dba8aa109841885f4e6afe0
sabbir hossain <shr472978@gmail.com>
2021-07-08 23:46:59 +0600

    text

--- src/components/Body/Ep.js
+++ src/components/Body/Ep.js
@@ -5,7 +5,7 @@ import Container from "@material-ui/core/Container";
 const Ep = () => {
     return (
         <Container>
-      <Typography variant="body1" color="error">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, qui ipsam. Dolores suscipit amet facere,
+      <Typography variant="body1" color="initial">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, qui ipsam. Dolores suscipit amet facere,
         ipsum nihil hic maiores cupiditate vitae, dolorum quos ab ea est placeat quo voluptas sint incidunt obcaecati rem, dolorem soluta facilis itaque harum sequi! 
         Libero officiis harum eaque! Debitis, architecto cumque. Est distinctio doloribus libero?</Typography>
     </Container>
