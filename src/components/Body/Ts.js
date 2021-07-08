bbeacfcb4c34ca2f4f598f9b6ae97149def7f4bd
sabbir hossain <shr472978@gmail.com>
2021-07-08 23:19:55 +0600

    up-ts

--- src/components/Body/Ts.js
+++ src/components/Body/Ts.js
@@ -3,7 +3,7 @@ import Container from "@material-ui/core/Container";
 const Ts = () => {
   return (
     <Container>
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, qui ipsam. Dolores suscipit amet facere, 
+      <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, qui ipsam. Dolores suscipit amet facere, 
         ipsum nihil hic maiores cupiditate vitae, dolorum quos ab ea est placeat quo voluptas sint incidunt obcaecati rem, dolorem soluta facilis itaque harum sequi! 
         Libero officiis harum eaque! Debitis, architecto cumque. Est distinctio doloribus libero?</Typography>
     </Container>
