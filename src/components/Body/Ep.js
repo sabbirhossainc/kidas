97014cac4bfb2d696f09cca90cf667c97ecff665
sabbir hossain <shr472978@gmail.com>
2021-07-09 15:17:57 +0600

    ep_up

--- src/components/Body/Ep.js
+++ src/components/Body/Ep.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 
 const Ep = () => {
     return (
         <Container>
-      <Typography variant="body1" color="initial">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, qui ipsam. Dolores suscipit amet facere,
+          <Card>
+          <CardBody>
+            <CardTitle tag="h3">
+              The Education programme of Keraniganj Intellectually Disabled And Autistic
+              School
+            </CardTitle>
+            <Typography variant="body1" color="initial">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, qui ipsam. Dolores suscipit amet facere,
         ipsum nihil hic maiores cupiditate vitae, dolorum quos ab ea est placeat quo voluptas sint incidunt obcaecati rem, dolorem soluta facilis itaque harum sequi! 
-        Libero officiis harum eaque! Debitis, architecto cumque. Est distinctio doloribus libero?</Typography>
+        Libero officiis harum eaque! Debitis, architecto cumque. Est distinctio doloribus libero?
+        </Typography>
+            <CardText />
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                ut placerat metus, consequat dictum elit. Vivamus id mi vitae
+                leo lobortis accumsan in a metus. Nam scelerisque, sapien quis
+                ultrices malesuada, tortor quam pellentesque elit, at viverra
+                nisi magna eget mauris. Suspendisse mollis metus eget convallis
+                aliquet. Nulla varius ipsum in metus pellentesque laoreet.
+                Curabitur ultricies tincidunt fermentum. Ut a odio consectetur,
+                convallis ante quis, auctor massa. Aliquam erat volutpat.
+                Praesent non lobortis libero. In lorem lorem, accumsan quis
+                blandit non, dapibus non sem. Sed sit amet nunc eget augue
+                interdum interdum. Proin non sem in ex efficitur dictum.
+              </Typography>
+            <CardText />
+          </CardBody>
+        </Card>
     </Container>
     )
 }
