8ec25b6733659adafd5b5018f330a0f4be03ea0a
sabbir hossain <shr472978@gmail.com>
2021-07-09 15:53:20 +0600

    tik

--- src/components/Body/Ticker.js
+++ src/components/Body/Ticker.js
@@ -3,15 +3,18 @@ import Ticker from "react-ticker";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 const MoveStuffAround = () => (
-  <Ticker offset="run-in" speed={10}>
-    {({ index }) => (
+  <Ticker offset="run-in" speed={8}>
+    {/* {({ index }) => ( */}
       <>
       <CssBaseline/>
         <p>
-          This is the Headline of element Lizards are a widespread group of squamate reptiles, with over #{index}! &nbsp;
+        অটিজম ও বুদ্ধি প্রতিবন্ধী বিশেষ শিশুদের স্বাস্থ্য ও শিক্ষা সেবা কার্যক্রম পরিচালনা করা জন্য ২০১৫ সালে অটিজম হেলথ 
+        কেয়ার সেন্টার প্রতিষ্ঠা করা হয়। অসহায় প্রতিবন্ধীদের শিক্ষা সেবা কার্যক্রম পরিচালনা করার জন্য কেরানীগঞ্জ বুদ্ধি প্রতিবন্ধী ও 
+        অটিস্টিক বিদ্যালয় প্রতিষ্ঠিত হয়েছে।  &nbsp;
+        {/* #{index}!  */}
         </p>
       </>
-    )}
+    {/* )} */}
   </Ticker>
 );
 
